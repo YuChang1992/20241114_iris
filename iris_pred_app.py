3a5198d2ea1514460261dba8b89c76a9a631e44f
YuChang1992 <carlworm25@gmail.com>
2025-02-14 14:44:12 +0800

    Update iris_pred_app.py

--- iris_pred_app.py
+++ iris_pred_app.py
@@ -2,8 +2,8 @@ import streamlit as st
 
 ps = st.navigation(
     [
-        st.Page("pages/page1.py", title="IRIS品種預測", icon="🌼"),  # path(路徑), title(名稱), (圖示)
-        st.Page("pages/page2.py", title="IRIS樣本分布", icon="📍")
+        st.Page("pages/page1.py", title="鳶尾花品種預測", icon="🌼"),  # path(路徑), title(名稱), (圖示)
+        st.Page("pages/page2.py", title="鳶尾花樣本分布", icon="📍")
     ]
 )
 
